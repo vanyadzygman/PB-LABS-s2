2f942200aa608cb1cb6daf1ef197752a059e87cb
Ivan <vanyadzygman@gmail.com>
2026-03-27 22:31:17 +0200

    fix LRU and LFU strategy code

--- lab3/strategies/lfu.js
+++ lab3/strategies/lfu.js
@@ -1,4 +1,13 @@
-export function lru(cache) {
-    const firstKey = cache.keys().next().value;
-    cache.delete(firstKey);
+export function lfu(cache) {
+    let minFreq = Infinity;
+    let minKey = null;
+
+    for (const [key, value] of cache) {
+    if (value.freq < minFreq) {
+        minFreq = value.freq;
+        minKey = key;
+    }
+}
+
+cache.delete(minKey);
 }
