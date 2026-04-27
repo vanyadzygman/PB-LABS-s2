e79a306db5f12d584e46a88c4ee8bd2ff60b3269
Ivan <vanyadzygman@gmail.com>
2026-04-27 20:59:49 +0300

    add request logging

--- lab8/proxy.js
+++ lab8/proxy.js
@@ -1,6 +1,7 @@
 export class AuthProxy {
     constructor(authStrategy) {
         this.authStrategy = authStrategy;
+        this.logs = [];
     }
 
     setAuth(authStrategy) {
@@ -9,6 +10,11 @@ export class AuthProxy {
 
     async request(url, options = {}) {
         const headers = this.authStrategy({ ...options.headers });
+
+        const logEntry = { url, headers, timestamp: new Date().toISOString() }
+        this.logs.push(logEntry);
+        console.log(`[${logEntry.timestamp}] requesting: ${url}`);
+
         const response = await fetch(url, { ...options, headers });
         return response.json();
     }
