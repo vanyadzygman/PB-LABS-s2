c97d149c22e9d69927c5b019ce0e9e95fb3372ba
Ivan <vanyadzygman@gmail.com>
2026-04-30 23:01:12 +0300

    add JSON formatter

--- lab9/index.js
+++ lab9/index.js
@@ -12,8 +12,16 @@ const failing = log({ level: 'ERROR' })(function failing() {
     throw new Error('something went wrong');
 });
 
+const multiply = log({
+    level: 'INFO',
+    formatter: (entry) => JSON.stringify(entry)
+})(function multiply(a, b) {
+    return a * b;
+});
+
 await add(2, 3);
 await fetchData(1);
+await multiply(4, 5);
 
 try {
     await failing();
